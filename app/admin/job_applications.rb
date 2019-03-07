6b5c56e916e26f671cebd1319e574d16014859f5
Dmitry Rocha <dmitryrck@gmail.com>
2019-03-07 13:55:47 +1300

    Update JobApplication.

--- app/admin/job_applications.rb
+++ app/admin/job_applications.rb
@@ -1,15 +1,18 @@
 ActiveAdmin.register JobApplication do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  permit_params :company_id, :job_id, :contact_id, :date_applied
 
+  form do |f|
+    inputs t("active_admin.details", model: resource_class) do
+      input :company
+      input :job
+      input :contact
+      input :website
+      input :url
+      input :still_acceptign_applications
+      input :refused
+      input :date_applied, as: :string
+    end
+
+    actions
+  end
 end
